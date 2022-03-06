7d2b4ada56f8023a71d4bf7fd087337571c5b455
smetto20 <sandra.metto@student.moringaschool.com>
2022-03-06 22:21:01 +0300

    ft-adds submit event listener

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var year= document.getElementById('year')
 var month= document.getElementById('month')
 var date = document.getElementById("date")
+
+var form = document.getElementById('form')
+form.addEventListener('submit',function(event){event.preventDefault() 
+Checker()
+validate()
+})
