a6eba1400b7fba5c1561a835d41816a4bb86b5c1
smetto20 <sandra.metto@student.moringaschool.com>
2022-03-06 22:22:36 +0300

    adds function to calculate day of the week

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-var year= document.getElementById('year')
-var month= document.getElementById('month')
-var date = document.getElementById("date")
+var year = document.getElementById("year");
+var month = document.getElementById("month");
+var date = document.getElementById("date");
 
-var form = document.getElementById('form')
-form.addEventListener('submit',function(event){event.preventDefault() 
-Checker()
-validate()
-})
+var form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  Checker();
+  validate();
+});
+
+function Checker() {
+  var yy = year.value.substring(2);
+  var cc = year.value.substring(0, 2);
+  var mm = month.value;
+  var dd = date.value;
+  d = parseInt(
+    (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7
+  ).toFixed();
+  console.log(d);
+  return d;
+}
